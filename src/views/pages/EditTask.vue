4eb6170d829d116beafce08dd5c0afb17ffffbde
mohamed zaitoune <115373788+ZaitouneMohamed@users.noreply.github.com>
2023-12-17 11:57:50 +0100

    Update EditTask.vue

--- src/views/pages/EditTask.vue
+++ src/views/pages/EditTask.vue
@@ -6,27 +6,30 @@
         <router-link class="btn btn-outline-info float-right" to="/">View All Tasks
         </router-link>
       </div>
-      <div class="card-body">
+      <div class="card-body" v-if="Task.is_owner">
         <form>
           <div class="form-group">
             <label>Title</label>
-            <input v-model="Task.title" type="text" class="form-control" />
+            <input v-model="Task.task.title" type="text" class="form-control" />
             <span class="text-danger" v-if="errors.title">{{ errors.title[0] }}</span>
           </div>
           <div class="form-group">
             <label htmlFor="description">Description</label>
-            <textarea v-model="Task.description" class="form-control" rows="3"></textarea>
+            <textarea v-model="Task.task.description" class="form-control" rows="3"></textarea>
             <span class="text-danger" v-if="errors.description">{{ errors.description[0] }}</span>
           </div>
           <div class="form-group">
             <label>Date</label>
-            <input v-model="Task.date" type="date" class="form-control" />
+            <input v-model="Task.task.date" type="date" class="form-control" />
             <span class="text-danger" v-if="errors.date">{{ errors.date[0] }}</span>
           </div>
           <button @click="handleSave()" :disabled="disabled" type="button" class="btn btn-outline-primary mt-3">
             Save Task
           </button>
         </form>
+      </div>
+      <div class="card-body" v-else>
+        <h1 className="text-center">This Task Is Not Your </h1>
       </div>
     </div>
   </layout-div>
@@ -44,11 +47,7 @@ export default {
   },
   data() {
     return {
-      Task: {
-        title: '',
-        description: '',
-        date: null,
-      },
+      Task: {},
       errors: {},
       token: JSON.parse(localStorage.getItem('token')),
       disabled: false,
@@ -63,7 +62,8 @@ export default {
     };
     axios.get(`https://task.electroniqueclasse.com/api/task/${id}`, config)
       .then(response => {
-        this.Task = response.data
+        this.Task = response.data,
+        console.log(response.data);
       })
       .catch(error => {
         Swal.fire({
@@ -93,7 +93,7 @@ export default {
           "Authorization": `Bearer ${this.token}`
         },
       };
-      axios.put(`https://task.electroniqueclasse.com/api/task/${id}`, this.Task, config)
+      axios.put(`https://task.electroniqueclasse.com/api/task/${id}`, this.Task.task, config)
         .then(response => {
           Swal.fire({
             icon: 'success',
